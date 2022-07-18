4d1e66103986e68196d0fc3837d8b56fe493bfa5
Ochanyoro <wadaoo0223@icloud.com>
2022-07-18 09:57:26 +0900

    Initial Commit

---
+++ tikTok/tikTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  tikTokApp.swift
+//  tikTok
+//
+//  Created by 大和田一裕 on 2022/07/18.
+//
+
+import SwiftUI
+
+@main
+struct tikTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
